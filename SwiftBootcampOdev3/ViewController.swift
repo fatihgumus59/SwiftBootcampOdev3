f7391a83be3a8d8f60cc1a77c0374394ed9118e9
Fatih Gumus <fatihgumus72@gmail.com>
2023-12-14 20:52:57 +0300

    Initial Commit

---
+++ SwiftBootcampOdev3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBootcampOdev3
+//
+//  Created by Fatih Gümüş on 14.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
